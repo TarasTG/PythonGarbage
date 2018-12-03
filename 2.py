7d4520d557309b047f9fdb833d35c65711a555d9
Taras Tolkachov <ttolkachev@softserveinc.com>
2018-12-03 11:45:17 +0200

    anges

--- 2.py
+++ 2.py
@@ -5,17 +5,33 @@ import re
 txt = ''
 
 to_replace = {'Реєстраційний номер\nоб’єкта нерухомого\nмайна:':'Реєстраційний номер об’єкта нерухомого майна:',
+              #'Актуальна інформація про об’єкт нерухомого майна': '',
               "Об’єкт нерухомого\nмайна":"Об’єкт нерухомого майна",
               "Відомості про права власності\n": "",
               "код ЄДРПОУ:": "код ЄДРПОУ - ",
               "(кв.м):": "(кв.м) - ",
-              "RRP-4HHL4ICLR\n": "",
+              "RRP-4HHOMCVNR\n": "",
               "країна реєстрації:": "країна реєстрації - "
 }
 
+csv_keys=[
+"Реєстраційний номер об’єкта нерухомого майна",
+"Об’єкт нерухомого майна",
+"Площа",
+"Адреса",
+"Номер запису про право власності",
+"Форма власності",
+"Розмір частки",
+"Власники",
+"Номер запису про право власності2",
+"Форма власності2",
+"Розмір частки2",
+"Власники2",
+]
+
 level_1 = "Реєстраційний номер об’єкта нерухомого майна"
 
-with open('input.txt','r') as my_file:  
+with open('input.txt', 'r') as my_file:
     txt = my_file.read()
     for key in to_replace:
         txt = txt.replace(key, to_replace[key])
@@ -33,40 +49,43 @@ with open('input.txt','r') as my_file:
                 result_1.append(result[i])
 
     result = result_1
-
     result_1 = []
 
     for elem in '\n'.join(result).split(level_1):
 
-        r = dict()
+        keys = list(s.split(':')[0] for s in elem.split('\n'))
+        new_keys = []
+        for key in keys:
+            cnt = 2
+            if key in new_keys:
+                key = key + str(cnt)
+                cnt += 1
+            new_keys.append(key)
 
+        new_dict = {}
+        cnt = 0
         for s in elem.split('\n'):
-            if len(s.split(':')) > 1:
-                r[s.split(':')[0]] = s.split(':')[1]
+            para = s.split(':')
+            if len(para) <= 1: continue
+            new_dict[new_keys[cnt]] = para[1]
+            cnt += 1
 
-        result_1.append(r)
+        result_1.append(new_dict)
 
     result = ""
-
-keys=[
-"Реєстраційний номер об’єкта нерухомого майна",
-"Об’єкт нерухомого майна",
-"Площа",
-"Адреса",
-"Номер запису про право власності",
-"Форма власності",
-"Розмір частки", 
-"Власники",
-    "Номер запису про право власності2",
-    "Форма власності2",
-    "Розмір частки2",
-    "Власники2",
-]
   
-result = '|'.join(keys)
+result = '|'.join(csv_keys)
 
-for r in result_1:
-    result = result + '|'.join(r.get(('' if k=="Реєстраційний номер об’єкта нерухомого майна" else k),'') for k in keys if k is not None) + '\n'
+result_1.pop(0)
+
+for record in result_1:
+    row = ''
+    for key in csv_keys:
+        row += record.get('' if key==csv_keys[0] else key, '') + '|'
+
+    result += '\n' + row
+
+#result+='\n'.join('|'.join(r.get(('' if k=="Реєстраційний номер об’єкта нерухомого майна" else k),'') for k in keys if k is not None for r in result_1))
 
     
 with open('out.csv','w') as my_file:
